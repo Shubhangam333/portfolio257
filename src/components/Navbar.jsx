5550924cca749714073b742539af9b6dccdd3ef9
Shubhangam333 <shubhangam27@gmail.com>
2024-04-11 13:40:30 +0530

    update navbar brand logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo-2.png";
+import logo from "../Assets/logo-3.jpg";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
@@ -39,9 +39,9 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-          {/* <p>Shubhangam Saxena</p> */}
+        <Navbar.Brand href="/" className="d-flex" style={{ marginTop: "10px" }}>
+          {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
+          <h4 style={{ color: "#c770f0" }}>Shubhangam Saxena</h4>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
